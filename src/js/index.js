97cbeba7cd4260c30f72e0cfdc9a45b0cdec3abc
victor <victot21@hotmail.com>
2022-03-19 16:07:13 -0300

    feat(Filter): html and css

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,31 @@
-console.log("Dev m3");
+let toggles = new Map();
+
+
+
+window.onload = () => {
+    const dropButton = document.querySelector(".dropdown");
+    const dropOption = document.querySelector(".dropdown-option");
+    dropButton.addEventListener("mouseover",() => {
+        dropOption.style.display = "block";
+    });
+    dropButton.addEventListener("mouseout",() => {
+        dropOption.style.display = "none";
+    });
+    dropOption.addEventListener("click",() => {
+        dropOption.style.display = "none";
+    });
+
+
+    const checkbox = document.querySelectorAll(".checkbox");
+
+    checkbox.forEach(checkbox => {
+        toggles.set(checkbox.id,false);
+        checkbox.addEventListener("click",() => {
+            const id = checkbox.id;
+            console.log(toggles)
+            toggles.set(id,!toggles.get(id));
+            checkbox.classList.toggle("checked",toggles.get(id));
+        });
+    });
+
+}
